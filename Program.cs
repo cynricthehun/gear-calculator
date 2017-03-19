6af404bb01599d672e48fb4e40c9b905bcd8fbc7
Admin <joeymeyer1986@gmail.com>
2017-03-19 00:10:30 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wow_gear_weight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
